f6c812944baa6ad246e10b1ba615ce174390282c
Sam Ahorro <135926765+samahorro@users.noreply.github.com>
2026-03-11 00:05:30 +0000

    change db name

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,6 +1,6 @@
 const { DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
-const sequelize = require('../config/database');
+const sequelize = require('../config/db');
 
 const User = sequelize.define('User', {
 
