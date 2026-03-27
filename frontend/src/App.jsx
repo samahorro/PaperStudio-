adc6c1082b291f468c9d547ed055c888dd06783d
Pauline Vu <129549236+paulinnev@users.noreply.github.com>
2026-03-27 13:15:06 -0700

    update app.jsx again

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,29 @@
-import{Routes, Route} from 'react-router-dom'
+import React, { useState } from 'react'
+import { createProduct } from './utils/api'
+import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import HomePage from './pages/HomePage'
 
 function App() {
-    return(
-      <>
-      <Navbar />
+  const [currentUser, setCurrentUser] = useState(null)
+
+  return (
+    <div style={{ backgroundColor: 'white', color: 'black', fontFamily: 'serif' }}>
+      
+      {/* Navbar always visible */}
+      <Navbar currentUser={currentUser} setCurrentUser={setCurrentUser} />
+
+      {/* Routes */}
       <Routes>
         <Route path="/" element={<HomePage />} />
+
+        {/* You can expand later like this */}
+        {/* <Route path="/products" element={<ProductsPage />} /> */}
+        {/* <Route path="/admin" element={<AdminPage />} /> */}
       </Routes>
-      </>
-    )
+
+    </div>
+  )
 }
 
 export default App
