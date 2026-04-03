2bc508b6f4243e774c7848c69c0348a992a8c8f9
Pauline Vu <129549236+paulinnev@users.noreply.github.com>
2026-04-03 12:26:26 -0700

    update product card

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -4,11 +4,11 @@ import './ProductCard.css'
 function ProductCard({ product }) {
 
   return (
-    <NavLink to={`/product/${product.id}`} className="product-card">
+    <NavLink to={`/products/${product.id}`} className="product-card">
       <div className="product-image-wrap">
         <img src={product.imageUrl} alt={product.name} className="product-image" />
         </div>
-        <h3 className="product-name">{product.name}</h3>
+        <p className="product-name">{product.name}</p>
         <p className="product-price">${product.price}</p>
     </NavLink>
   )
