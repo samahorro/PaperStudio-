9bacc5f398137f89f85630d23bae871b153af92b
Pauline Vu <129549236+paulinnev@users.noreply.github.com>
2026-04-05 12:10:47 -0700

    vite config testing

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
     proxy: {
-      '/api': 'http://localhost:5000'
+      '/api': {
+        target: 'http://paperstudio-env.eba-zhfvtb4v.us-east-1.elasticbeanstalk.com',
+        changeOrigin: true,
+        secure: false,
+      }
     }
   }
 })
